69d37e6a68d2c68f77505c90a3e70e415c29b2ff
Tarek <tare2fo2ad@gmail.com>
2024-09-10 13:39:12 +0300

    updates

---
+++ script.js
@@ -0,0 +1,28 @@
+const filterBtn = document.querySelector(".filter-btn");
+const closeFilterBtn = document.querySelector(".close-filter i");
+
+filterBtn?.addEventListener("click", function () {
+  document.querySelector(".filter-layout")?.classList.remove("hide");
+  document
+    .querySelector(".form-filter")
+    ?.classList.add("form-filter-animation");
+});
+
+closeFilterBtn.addEventListener("click", function () {
+  document.querySelector(".filter-layout")?.classList.add("hide");
+  document
+    .querySelector(".form-filter")
+    ?.classList.remove("form-filter-animation");
+});
+
+document
+  .querySelector(".filter-layout")
+  .addEventListener("click", function (event) {
+    const formFilter = document.querySelector(".form-filter");
+    if (formFilter && !formFilter.contains(event.target)) {
+      document.querySelector(".filter-layout")?.classList.add("hide");
+      document
+        .querySelector(".form-filter")
+        ?.classList.remove("form-filter-animation");
+    }
+  });
